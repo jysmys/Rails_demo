7e16f22b89dd6ef1b68e8a0b856972cd6527a0d3
japh <jenny.scherr@svenskaspel.se>
2020-04-24 16:32:43 +0200

    added navbar so the user can login

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,7 @@
+//= require jquery
+//= require bootstrap-sprockets
+//= require jquery_ujs
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
-require("semantic-ui");
