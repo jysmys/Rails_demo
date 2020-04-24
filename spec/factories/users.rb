8107f25f1b9d729ebc18586ffea168448cec37cd
japh <jenny.scherr@svenskaspel.se>
2020-04-24 17:27:54 +0200

    migrate user name to db. After unit test failed to get name

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+      name {"Humlan"}
+      email {"biet@gmail.com"} 
+      password {"zumzum2020"}
+      password_confirmation {"zumzum2020"}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
-require 'rails_helper'
+RSpec.describe Comment, type: :model do
+  describe 'DB table' do
+    it { is_expected.to have_db_column :name }
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :encrypted_password }
+  end
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :encrypted_password }
+  end
+
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(create(:user)).to be_valid
+    end
+  end
 end
+
+  
