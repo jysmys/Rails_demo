37f01c1e6e2f4e2f976179c81479b15a99e01486
japh <jenny.scherr@svenskaspel.se>
2020-04-24 17:11:40 +0200

    unit test added for the comment table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :name, length: { maximum: 20 }
+  validates :name, presence: true
+  validates_uniqueness_of :name
+
+  validates :encrypted_password, length: { minimum: 6 }
+  validates :encrypted_password, presence: true
+  
 end
